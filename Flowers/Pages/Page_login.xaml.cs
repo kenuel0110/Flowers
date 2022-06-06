6acdd47db5cc77dab57c13e96af36162b31124e1
kenuel0110 <zeroonezero0110@gmail.com>
2022-06-07 02:44:20 +0300

    Авторизация готова

--- Flowers/Pages/Page_login.xaml.cs
+++ Flowers/Pages/Page_login.xaml.cs
@@ -20,9 +20,48 @@ namespace Flowers.Pages
     /// </summary>
     public partial class Page_login : Page
     {
+
+        List<User> users = Classes.Manager.DBContext.User.ToList();
+        List<Role> roles = Classes.Manager.DBContext.Role.ToList();
         public Page_login()
         {
             InitializeComponent();
+        }
+
+        private void btn_login_guest_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            NavigationService.Navigate(new Pages.Page_main("Гость", "Гость"));
+        }
+
+        private void btn_login_Click(object sender, RoutedEventArgs e)
+        {
+            string login = tb_login.Text;
+            string password = tb_password.Password;
+            bool auth = false;
+            string fullname = "";
+            string role = "";
+
+            if (login == "" || password == "")
+            {
+                MessageBox.Show("ЗАполните поля");
+            }
+            else
+            {
+                foreach (var item in this.users)
+                {
+                    if (item.UserLogin == login && item.UserPassword == password)
+                    {
+                        auth = true;
+                        fullname = $"{item.UserSurname} {item.UserName} {item.UserPatronymic}";
+                        role = item.UserRole.ToString();
+                        break;
+                    }
+                }
+                if (auth == false)
+                    MessageBox.Show("Неверный логин или пароль");
+                else
+                    NavigationService.Navigate(new Pages.Page_main(fullname, role));
+            }
         }
     }
 }
